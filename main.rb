5ce09e7cacecf5bcfc73ad586967b6082028f126
Yuda <yuda_p411@apps.ipb.ac.id>
2024-12-26 18:41:37 +0700

    26/12/2024

--- main.rb
+++ main.rb
@@ -7,8 +7,16 @@ SQLiteDatabase.new.drop_users_table_sql
 class App
   def call(env)
     request = Rack::Request.new(env)
+    if request.request_method == 'OPTIONS'
+      return [200, { 'Access-Control-Allow-Origin' => '*',
+                     'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+                     'Access-Control-Allow-Headers' => 'Content-Type' }, []]
+    end
     handler = UserHandler.new
     response = handler.handle_request(request)
+    response[:headers]['Access-Control-Allow-Origin'] = '*'
+    response[:headers]['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+    response[:headers]['Access-Control-Allow-Headers'] = 'Content-Type'
     [
       response[:status],
       response[:headers],
